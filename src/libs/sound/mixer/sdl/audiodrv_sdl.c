172c3f4a635bcb9f014425bd573a1a9790ed9226
Ala-lala <>
2019-10-24 16:38:37 -0700

    Make MixSDL use new audio API in SDL2 and allow format conversion
    This fixes MixSDL on windows; previously, it was broken since SDL2 could only provide 32-bit float samples natively with WASAPI.

--- src/libs/sound/mixer/sdl/audiodrv_sdl.c
+++ src/libs/sound/mixer/sdl/audiodrv_sdl.c
@@ -24,6 +24,10 @@
 #include <stdlib.h>
 
 
+#if SDL_MAJOR_VERSION > 1
+static SDL_AudioDeviceID dev;
+#endif
+
 static const audio_Driver mixSDL_Driver =
 {
 	mixSDL_Uninit,
@@ -184,7 +188,28 @@ mixSDL_Init (audio_Driver *driver, sint32 flags)
 	desired.callback = audioCallback;
 	
 	log_add (log_Info, "Opening SDL audio device.");
+#if SDL_MAJOR_VERSION > 1
+	dev = SDL_OpenAudioDevice (NULL, 0, &desired, &obtained,
+			SDL_AUDIO_ALLOW_FREQUENCY_CHANGE | SDL_AUDIO_ALLOW_CHANNELS_CHANGE
+#ifdef SDL_AUDIO_ALLOW_SAMPLES_CHANGE
+			| SDL_AUDIO_ALLOW_SAMPLES_CHANGE
+#endif
+			);
+	if (dev != 0 && obtained.channels != 1 && obtained.channels != 2)
+	{
+		// try again without SDL_AUDIO_ALLOW_CHANNELS_CHANGE
+		// in case the device only supports >2 channels for some reason
+		dev = SDL_OpenAudioDevice (NULL, 0, &desired, &obtained,
+				SDL_AUDIO_ALLOW_FREQUENCY_CHANGE
+#ifdef SDL_AUDIO_ALLOW_SAMPLES_CHANGE
+				| SDL_AUDIO_ALLOW_SAMPLES_CHANGE
+#endif
+				);
+	}
+	if (dev == 0)
+#else
 	if (SDL_OpenAudio (&desired, &obtained) < 0)
+#endif
 	{
 		log_add (log_Error, "Unable to open audio device: %s",
 				SDL_GetError ());
@@ -195,7 +220,11 @@ mixSDL_Init (audio_Driver *driver, sint32 flags)
 		(obtained.channels != 1 && obtained.channels != 2))
 	{
 		log_add (log_Error, "Unable to obtain desired audio format.");
+#if SDL_MAJOR_VERSION > 1
+		SDL_CloseAudioDevice (dev);
+#else
 		SDL_CloseAudio ();
+#endif
 		SDL_QuitSubSystem (SDL_INIT_AUDIO);
 		return -1;
 	}
@@ -220,7 +249,11 @@ mixSDL_Init (audio_Driver *driver, sint32 flags)
 	{
 		log_add (log_Error, "Mixer initialization failed: %x",
 				mixer_GetError ());
+#if SDL_MAJOR_VERSION > 1
+		SDL_CloseAudioDevice (dev);
+#else
 		SDL_CloseAudio ();
+#endif
 		SDL_QuitSubSystem (SDL_INIT_AUDIO);
 		return -1;
 	}
@@ -230,7 +263,11 @@ mixSDL_Init (audio_Driver *driver, sint32 flags)
 	if (SoundDecoder_Init (flags, &formats))
 	{
 		log_add (log_Error, "Sound decoders initialization failed.");
+#if SDL_MAJOR_VERSION > 1
+		SDL_CloseAudioDevice (dev);
+#else
 		SDL_CloseAudio ();
+#endif
 		mixer_Uninit ();
 		SDL_QuitSubSystem (SDL_INIT_AUDIO);
 		return -1;
@@ -248,14 +285,22 @@ mixSDL_Init (audio_Driver *driver, sint32 flags)
 	{
 		log_add (log_Error, "Stream decoder initialization failed.");
 		// TODO: cleanup source mutexes [or is it "muti"? :) ]
+#if SDL_MAJOR_VERSION > 1
+		SDL_CloseAudioDevice (dev);
+#else
 		SDL_CloseAudio ();
+#endif
 		SoundDecoder_Uninit ();
 		mixer_Uninit ();
 		SDL_QuitSubSystem (SDL_INIT_AUDIO);
 		return -1;
 	}
 
+#if SDL_MAJOR_VERSION > 1
+	SDL_PauseAudioDevice (dev, 0);
+#else
 	SDL_PauseAudio (0);
+#endif
 		
 	return 0;
 }
@@ -285,7 +330,11 @@ mixSDL_Uninit (void)
 		mixSDL_DeleteSources (1, &soundSource[i].handle);
 	}
 
+#if SDL_MAJOR_VERSION > 1
+	SDL_CloseAudioDevice (dev);
+#else
 	SDL_CloseAudio ();
+#endif
 	mixer_Uninit ();
 	SoundDecoder_Uninit ();
 	SDL_QuitSubSystem (SDL_INIT_AUDIO);
