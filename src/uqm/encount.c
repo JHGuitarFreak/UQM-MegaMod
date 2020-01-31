9e0b81e9fd39be98571968b3c5afe815de0b3b74
Serosis <serosis@gmail.com>
2020-01-31 04:14:17 -0800

    HD: Fixed Flagship port/starboard weapon offsets

--- src/uqm/encount.c
+++ src/uqm/encount.c
@@ -882,6 +882,8 @@ EncounterBattle (void)
 	if(DIF_EASY)
 		PlayerControl[1] = CYBORG_CONTROL | STANDARD_RATING;
 
+	// PlayerControl[1] = HUMAN_CONTROL | STANDARD_RATING; // Serosis: Yes, you can make Adventure mode 2-player
+
 	GameSounds = CaptureSound (LoadSound (GAME_SOUNDS));
 
 	Battle (NULL);
