b4ac6d716e0252cd2913668e9bd6b6269fc20ef1
Serosis <serosis@gmail.com>
2021-03-18 12:53:12 -0700

    Remove commented code that is no longer needed

--- src/uqm/shipyard.c
+++ src/uqm/shipyard.c
@@ -1490,9 +1490,6 @@ DrawBluePrint (MENU_STATE *pMS)
 
 				
 			GetFTankCapacity (&r.corner);
-			//log_add(log_Debug, "volume on %u, hefueltankcapacity %u", volume, HEFUEL_TANK_CAPACITY);
-			//r.corner.y -= volume == HEFUEL_TANK_CAPACITY ? IF_HD(19) : IF_HD(28);
-			//r.corner.x += volume == HEFUEL_TANK_CAPACITY ? IF_HD(2) : IF_HD(1);
 
 			r.extent.width = RES_SCALE(5);
 			DrawFilledRectangle(&r);
