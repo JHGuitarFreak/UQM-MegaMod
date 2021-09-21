JHGuitarFreak/UQM-MegaMod

commit 834166e864fecdf91e815b52c284fb1ab7bb3c7a

HD: Horizontally align text on the Outfit screen

src/uqm/sis.c
@@ -286,17 +286,14 @@ DrawSISMessageEx (const UNICODE *pStr, SIZE CurPos, SIZE ExPos, COUNT flags)
 					}
 				}
 				break;
-
-				
 		}
-
 	}
 
 	if (!(flags & DSME_MYCOLOR))
 		SetContextForeGroundColor (SIS_MESSAGE_TEXT_COLOR);
 
 	t.baseline.y = SIS_MESSAGE_HEIGHT - RES_SCALE(2);
-	t.baseline.x = (SIS_MESSAGE_WIDTH >> 1);
+	t.baseline.x = RES_SCALE (RES_DESCALE (SIS_MESSAGE_WIDTH) >> 1);
 	t.pStr = pStr;
 	t.CharCount = (COUNT)~0;
 	SetContextFont (TinyFont);
@@ -339,7 +336,7 @@ DrawSISMessageEx (const UNICODE *pStr, SIZE CurPos, SIZE ExPos, COUNT flags)
 		}
 
 		ClearDrawable ();
-		DrawBorder(2, FALSE);
+		DrawBorder (2, FALSE);
 		SetCursorFlashBlock (FALSE);
 
 
@@ -601,7 +598,7 @@ DrawCaptainsName (bool NewGame)
 void
 DrawFlagshipName (BOOLEAN InStatusArea, bool NewGame)
 {
-	RECT r;
+	RECT r, rHD;
 	TEXT t;
 	FONT OldFont;
 	Color OldColor;
@@ -633,8 +630,8 @@ DrawFlagshipName (BOOLEAN InStatusArea, bool NewGame)
 
 		if (IS_HD)
 		{
-			r.extent.width = SIS_SCREEN_WIDTH * 0.75;
-			r.corner.x = (SIS_SCREEN_WIDTH - r.extent.width) / 2;
+			rHD.extent.width = SIS_SCREEN_WIDTH * 0.75;
+			rHD.corner.x = (SIS_SCREEN_WIDTH - r.extent.width) / 2;
 		}
 
 		t.pStr = buf;
@@ -660,13 +657,13 @@ DrawFlagshipName (BOOLEAN InStatusArea, bool NewGame)
 	}
 	OldFontEffect = SetContextFontEffect (NULL);
 	OldColor = SetContextForeGroundColor (FLAGSHIP_NAME_BACKGROUND_COLOR);
-	DrawFilledRectangle (&r);
+	DrawFilledRectangle (RES_BOOL (&r, &rHD));
 
 	if (!NewGame)
 		DrawBorder(12, FALSE);
 
-	t.baseline.x = r.corner.x + (r.extent.width >> 1);
-	t.baseline.y = r.corner.y + (SHIP_NAME_HEIGHT - RES_SCALE(InStatusArea));
+	t.baseline.x = r.corner.x + RES_SCALE (RES_DESCALE (r.extent.width) >> 1);
+	t.baseline.y = r.corner.y + (SHIP_NAME_HEIGHT - RES_SCALE (InStatusArea));
 	t.align = ALIGN_CENTER;
 	t.CharCount = (COUNT)~0;
 	if (optWhichFonts == OPT_PC)

commit ee6c736c83ad39c32d22882664fe66639b79b63d

HD: Fix for the red box remaining after entering a Flagship name
Caused by commit 834166e864fecdf91e815b52c284fb1ab7bb3c7a

src/uqm/sis.c
@@ -606,6 +606,9 @@ DrawFlagshipName (BOOLEAN InStatusArea, bool NewGame)
 	FRAME OldFontEffect;
 	UNICODE buf[250];
 
+	OldFontEffect = SetContextFontEffect (NULL);
+	OldColor = SetContextForeGroundColor (FLAGSHIP_NAME_BACKGROUND_COLOR);
+
 	if (InStatusArea)
 	{
 		OldContext = SetContext (StatusContext);
@@ -617,6 +620,8 @@ DrawFlagshipName (BOOLEAN InStatusArea, bool NewGame)
 		r.extent.height = SHIP_NAME_HEIGHT;
 
 		t.pStr = GLOBAL_SIS (ShipName);
+
+		DrawFilledRectangle (&r);
 	}
 	else
 	{
@@ -627,12 +632,6 @@ DrawFlagshipName (BOOLEAN InStatusArea, bool NewGame)
 		r.corner.y = RES_SCALE(1);
 		r.extent.width = SIS_SCREEN_WIDTH;
 		r.extent.height = SHIP_NAME_HEIGHT;
-
-		if (IS_HD)
-		{
-			rHD.extent.width = SIS_SCREEN_WIDTH * 0.75;
-			rHD.corner.x = (SIS_SCREEN_WIDTH - r.extent.width) / 2;
-		}
 
 		t.pStr = buf;
 		snprintf (buf, sizeof buf, "%s %s",
@@ -654,10 +653,15 @@ DrawFlagshipName (BOOLEAN InStatusArea, bool NewGame)
 				ptr++;
 			}
 		}
+
+		if (IS_HD)
+		{
+			rHD.extent.width = SIS_SCREEN_WIDTH * 0.75;
+			rHD.corner.x = (SIS_SCREEN_WIDTH - r.extent.width) / 2;
+		}
+
+		DrawFilledRectangle (RES_BOOL (&r, &rHD));
 	}
-	OldFontEffect = SetContextFontEffect (NULL);
-	OldColor = SetContextForeGroundColor (FLAGSHIP_NAME_BACKGROUND_COLOR);
-	DrawFilledRectangle (RES_BOOL (&r, &rHD));
 
 	if (!NewGame)
 		DrawBorder(12, FALSE);