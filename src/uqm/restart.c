3c8ede8b49eafb5a76dc8a56c4dc01b8aebe1de5
JHGuitarFreak <jhguitarfreak@gmail.com>
2022-04-29 02:45:55 -0700

    Remove references to "StarConFont" in restart.c as they're not needed

--- src/uqm/restart.c
+++ src/uqm/restart.c
@@ -292,7 +292,6 @@ DrawRestartMenuGraphic (MENU_STATE *pMS)
 	{
 		DestroyFont (TinyFont);
 		DestroyFont (PlyrFont);
-		DestroyFont (StarConFont);
 		if (pMS->CurFrame)
 		{
 			DestroyDrawable (ReleaseDrawable (pMS->CurFrame));
@@ -307,7 +306,6 @@ DrawRestartMenuGraphic (MENU_STATE *pMS)
 		{
 			TinyFont = LoadFont (TINY_FONT_FB);
 			PlyrFont = LoadFont (PLAYER_FONT_FB);
-			StarConFont = LoadFont (STARCON_FONT_FB);
 		}
 		if (pMS->CurFrame == 0)
 			pMS->CurFrame = CaptureDrawable (
@@ -319,7 +317,6 @@ DrawRestartMenuGraphic (MENU_STATE *pMS)
 		{
 			TinyFont = LoadFont (TINY_FONT_HD);
 			PlyrFont = LoadFont (PLAYER_FONT_HD);
-			StarConFont = LoadFont (STARCON_FONT_HD);
 		}
 		if (pMS->CurFrame == 0)
 			pMS->CurFrame = CaptureDrawable (
