d6bc0a4191b53c43817cddfcae7af7c49d067c55
Kruzen <68859542+Kruzen@users.noreply.github.com>
2022-10-26 13:12:52 +0400

    Fixed hypothetical bug if Androsynth will fail to transform in comet form

--- src/uqm/ships/androsyn/androsyn.c
+++ src/uqm/ships/androsyn/androsyn.c
@@ -421,6 +421,14 @@ androsynth_postprocess (ELEMENT *ElementPtr)
 			StarShipPtr->energy_counter = 1;
 		}
 	}
+	else if (StarShipPtr->cur_status_flags & SPECIAL)
+				/* Kruzen: If we failed to transform into
+				 * comet due to collision failed in Process() -
+				 * remove the special flag.
+				 */
+	{
+		StarShipPtr->cur_status_flags &= ~ANDROSYN_COMET_TOGGLE;
+	}
 }
 
 static void
