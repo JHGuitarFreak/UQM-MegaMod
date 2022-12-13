a754c9d6893349488f98013b48d90e261ecaf8c1
Kruzen <68859542+Kruzen@users.noreply.github.com>
2022-12-13 17:37:44 +0400

    Fixed Thraddash being friendly after the player takes Aqua Helix and EXTENDED is off

--- src/uqm/comm/thradd/thraddc.c
+++ src/uqm/comm/thradd/thraddc.c
@@ -704,7 +704,7 @@ Intro (void)
 		return;
 	}
 
-	if (GET_GAME_STATE(AQUA_HELIX) && (EXTENDED && ThraddPtr->allied_state != GOOD_GUY))
+	if (GET_GAME_STATE (AQUA_HELIX) && !(EXTENDED && ThraddPtr->allied_state == GOOD_GUY))
 	{
 		NumVisits = GET_GAME_STATE (HELIX_VISITS);
 		switch (NumVisits++)
