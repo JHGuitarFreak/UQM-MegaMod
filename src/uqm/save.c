ef918819c88799c04ba4567982e3dfbcce44acea
JHGuitarFreak <jhguitarfreak@gmail.com>
2024-03-22 11:58:05 -0700

    Clean up vestigial homeworld code

--- src/uqm/save.c
+++ src/uqm/save.c
@@ -733,29 +733,6 @@ const GameStateBitMap gameStateBitMap[] = {
 	{ "SYS_VISITED_14", 32 },
 	{ "SYS_VISITED_15", 32 },
 
-	// Instead of this
-
-	//{ "KNOW_CHMMR_HOMEWORLD", 1 },
-	//{ "KNOW_ORZ_HOMEWORLD", 1 },
-	//{ "KNOW_PKUNK_HOMEWORLD", 1 },
-	//{ "KNOW_SHOFIXTI_HOMEWORLD", 1 },
-	//{ "KNOW_SPATHI_HOMEWORLD", 1 },
-	//{ "KNOW_SUPOX_HOMEWORLD", 1 },
-	//{ "KNOW_THRADD_HOMEWORLD", 1 },
-	//{ "KNOW_UTWIG_HOMEWORLD", 1 },
-	//{ "KNOW_VUX_HOMEWORLD", 1 },
-	//{ "KNOW_YEHAT_HOMEWORLD", 1 },
-	//{ "KNOW_DRUUGE_HOMEWORLD", 1 },
-	//{ "KNOW_ILWRATH_HOMEWORLD", 1 },
-	//{ "KNOW_MYCON_HOMEWORLD", 1 },
-	//{ "KNOW_SLYLANDRO_HOMEWORLD", 1 },
-	//{ "KNOW_UMGAH_HOMEWORLD", 1 },
-	//{ "KNOW_ZOQFOT_HOMEWORLD", 1 },
-	//{ "KNOW_SYREEN_HOMEWORLD", 1 },
-	//{ "KNOW_ANDROSYNTH_HOMEWORLD", 1 },
-
-	// It'll be this
-
 	{ "KNOW_HOMEWORLD", 18 },
 
 	{ "HM_ENCOUNTERS", 9 },
