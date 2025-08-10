122565541848a24b33a761855add947722ab32c3
JHGuitarFreak <jhguitarfreak@gmail.com>
2025-08-10 15:14:30 -0500

    Final pass on new font2png python script
    Removed the old ttf2png script

--- tools/font-renderer/font2png.py
+++ tools/font-renderer/font2png.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-# Created with help from DeepSeek R1
+# Created with help from DeepSeek R1 (Mainly to help me understand how to do things but not generating the entire code)
 # Lists out fonts to choose from in the current working directory to export each individual glyph as PNG in your chosen point size
 # PNG named after their unicode desgnations in hex format E.G. "0004e.png"
+# Requires the FontForge and Pillow packages
 
 import sys
 import glob
@@ -13,175 +14,119 @@ from PIL import Image
 from PIL import ImageOps
 from pathlib import Path
 
-# Find all font files in the working directory
+cRed     = "\033[31m"
+cGreen   = "\033[32m"
+cYellow  = "\033[33m"
+cBlue    = "\033[34m"
+cMagenta = "\033[35m"
+cCyan    = "\033[36m"
+cGray    = "\033[90m"
+tReset   = "\033[0m"
+tBold    = "\033[1m"
+
 Files = glob.glob('*.ttf') + glob.glob('*.otf') + glob.glob('*.woff')
 NumFiles = len(Files)
 
 if NumFiles == 0:
-	sys.exit("Terminating script because there are no usable font files in the current directory.")
+	sys.exit(f"{cRed}Terminating script because there are no usable font files in the current directory.")
 
 parser = argparse.ArgumentParser(description="Export font glyphs as individual PNG files", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--ansi", action="store_true", help="export only ANSI character set")
 parser.add_argument("--ascii", action="store_true", help="export only the basic ASCII character set")
 parser.add_argument("--invert", action="store_false", help="export PNG as black on white")
+parser.add_argument("--ascender", type=int, help="Custom ascender value for stubbornly cut off tops of fonts")
+parser.add_argument("--descender", type=int, help="Custom descender value for stubbornly cut off bottoms of fonts")
+parser.add_argument("--uqm", action="store_true", help="Takes 100 off the right side bearing of countour-less glyphs (Removes all side bearings otherwise), takes 1 pixel off the final width")
 args = parser.parse_args()
 config = vars(args)
 
 if args.ansi and args.ascii:
-	sys.exit("Arguments --ansi and --ascii can not be used together. Script will now exit.")
+	sys.exit(f"{tBold}{cRed}ERROR! {cYellow}Arguments --ansi and --ascii can not be used together. Script will now exit.")
 
-print('\nPick a font from the list to work with...\n')
+print(f'{cCyan}\nPick a font from the list to work with...\n{tReset}')
 
 for i in range (0, NumFiles):
-	print('\t' + str(i) + ") " + Files[i])
+	colour = cGray if i % 2 == 0 else tReset
+	print(f'{colour}\t{i:>3}) {Files[i]}{tReset}')
 
 print('')
 
-# Have the user pick from a list of found font files
 UserInput = -1
 while not int(UserInput) in range(0, NumFiles):
-	UserInput = input("Choose between (0 - " + str(NumFiles - 1) + ") : ")
+	UserInput = input(f"{cCyan}Choose between ({tReset}0 - {NumFiles - 1}{cCyan}) : {tReset}")
 
-FontPath = Path(Files[int(UserInput)]) # Fill in FontPath with the user selected font
+FontPath = Path(Files[int(UserInput)])
 
 print('')
 
-# Have the user enter the point size they want the font to be exported as
-FontSize = int(input("Enter Font Size In Point: "))
+FontSize = int(input(f"{cCyan}Enter Font Size In Point: {tReset}"))
 
-print('\nExporting ' + Files[int(UserInput)] + ' with a size of ' + str(FontSize) + 'pt\n')
+print(f"{cGreen}\nExporting {tReset}{Files[int(UserInput)]} {cGreen}with a size of {tReset}{FontSize}{cGreen}pt\n{cYellow}")
 
-FontStem = Path(FontPath.stem)              # Set the font name
-FontStem.mkdir(parents=True, exist_ok=True) # Create the output directory if it doesn't exist
-Font = fontforge.open(FontPath.name, 1)     # Open the font file
+FontStem = Path(FontPath.stem)
+FontStem.mkdir(parents=True, exist_ok=True)
+Font = fontforge.open(FontPath.name, 1)
 
-# Calculate the scaling factor for the desired point size
 emSize = Font.em
 ScaleFactor = FontSize / emSize
+LowestOffs = None
+WidthAdjust = 1 if args.uqm else 0
 
-print (ScaleFactor)
+if args.ascender is not None and args.ascender < Font.hhea_ascent:
+	sys.exit(f"{tBold}{cRed}\nERROR! {cYellow}Argument --ascender ({args.ascender}) shorter than the selected font's Ascender ({Font.hhea_ascent}). Script will now exit.")
+if args.descender is not None and args.descender > Font.hhea_descent:
+	sys.exit(f"{tBold}{cRed}\nERROR! {cYellow}Argument --descender ({args.descender}) taller than the selected font's Descender ({Font.hhea_descent}). Script will now exit.")
 
-# Initialize variables to track the highest and lowest points
-HighestPoint = None
-LowestPoint = None
-max_height = 0
+Ascender = args.ascender if args.ascender else Font.hhea_ascent
+Descender = args.descender if args.descender else Font.hhea_descent
 
-# Iterate through all glyphs in the font
+ImgHeight = int((Ascender - Descender) * ScaleFactor)
+
+values = [Ascender, Descender, ImgHeight]
+max_width = max(len(str(x)) for x in values)
+
+print(f"\n"
+		f"{cMagenta}Ascender:  {tReset}{Ascender:>{max_width}}\n"
+		f"{cMagenta}Descender: {tReset}{Descender:>{max_width}}\n"
+		f"{cMagenta}ImgHeight: {tReset}{ImgHeight:>{max_width}}\n"
+		f"{tReset}"
+)
+
 for Glyph in Font.glyphs():
 	if not Glyph.isWorthOutputting():
 		continue
 
-	# Get the glyph's Unicode code point
 	UnicodeValue = Glyph.unicode
-	
-	# Skip glyphs below the space character
-	if UnicodeValue < 0x20:
+
+	if UnicodeValue < 0x20 or (args.ascii and UnicodeValue > 0x7F) or (args.ansi and UnicodeValue > 0xFF):
 		continue
 
-	if (args.ascii and UnicodeValue > 0x7F) or (args.ansi and UnicodeValue > 0xFF):
-		continue
-
-	height = (Glyph.boundingBox()[3] - Glyph.boundingBox()[1]) * ScaleFactor
-
-	#pixels_per_unit = (dpi * math.pow((point_size / 72), 2))
-
-	if height is None or height > max_height:
-		max_height = height
-
-	#print (f"{UnicodeValue:05x} -> height: {height}")
-
-	# Iterate through all contours in the glyph
-	for Contour in Glyph.foreground:
-		# Iterate through all points in the contour
-		for Point in Contour:
-			y = Point.y
-			
-			# Update highest point
-			if HighestPoint is None or y > HighestPoint:
-				HighestPoint = y
-			
-			# Update lowest point
-			if LowestPoint is None or y < LowestPoint:
-				LowestPoint = y
-
-
-# Calculate the difference in height
-HeightDiff = int((HighestPoint - LowestPoint) * ScaleFactor)
-
-print (f"HighestPoint={HighestPoint}, LowestPoint={LowestPoint}, HeightDiff={HeightDiff}")
-
-LowestOffs = None;
-
-for Glyph in Font.glyphs():
-	if Glyph.isWorthOutputting():
-
-		# Get the glyph's Unicode code point
-		UnicodeValue = Glyph.unicode
-		
-		# Skip glyphs below the space character
-		if UnicodeValue < 0x20:
-			continue
-
-		if (args.ascii and UnicodeValue > 0x7F) or (args.ansi and UnicodeValue > 0xFF):
-			continue
-
-		# Remove left and right side bearings (side metrics) for glyphs with contours
+	if args.uqm:
 		if not Glyph.layers['Fore'].isEmpty():
 			Glyph.left_side_bearing = 0
 			Glyph.right_side_bearing = 0
-		
-		# Generate the output file name
-		OutputFile = str(FontStem.joinpath(f"{UnicodeValue:05x}.png"))
-		
-		# Export the glyph as a PNG image
-		Glyph.export(OutputFile, FontSize)
-
-		Img = Image.open(OutputFile) # Open the exported image for more operations
-
-		#print (f"{UnicodeValue:05x} -> Img.height: {Img.height}")
-
-		# Calculate vertical offset to center the glyph
-		Offset = (HeightDiff - Img.height) // 2
-
-		if LowestOffs is None or Offset < LowestOffs:
-			LowestOffs = Offset;
-
-for Glyph in Font.glyphs():
-	if Glyph.isWorthOutputting():
-
-		# Get the glyph's Unicode code point
-		UnicodeValue = Glyph.unicode
-		
-		# Skip glyphs below the space character
-		if UnicodeValue < 0x20:
-			continue
-
-		if (args.ascii and UnicodeValue > 0x7F) or (args.ansi and UnicodeValue > 0xFF):
-			continue
-		
-		# Generate the output file name
-		OutputFile = str(FontStem.joinpath(f"{UnicodeValue:05x}.png"))
-
-		Img = Image.open(OutputFile)   # Open the exported PNG with Pillow
-		Img = Img.convert("L")         # Convert the image to 8bit Grayscale
-
-		if args.invert:
-			Img = ImageOps.invert(Img) # invert the image
-			Color = "Black"
 		else:
-			Color = "White"
+			if Glyph.right_side_bearing > 100:
+				Glyph.right_side_bearing = int(Glyph.right_side_bearing) - 100
 
-		# Calculate vertical offset to center the glyph
-		Offset = (HeightDiff - Img.height) // 2
+	OutputFile = str(FontStem.joinpath(f"{UnicodeValue:05x}.png"))
+	Glyph.export(OutputFile, FontSize)
 
-		# Create a new 8bit grayscale canvas with the original width, calculated height, and black background
-		NewImg = Image.new("L", (Img.width, HeightDiff), Color)
+	Img = Image.open(OutputFile)
+	Img = Img.convert("L")
 
-		# Paste the glyph onto the new canvas
-		NewImg.paste(Img, (0, Offset - abs(LowestOffs)))
+	if args.invert:
+		Img = ImageOps.invert(Img)
+		Color = "Black"
+	else:
+		Color = "White"
 
-		# Save the final image
-		NewImg.save(OutputFile)
+	Offset = (ImgHeight - Img.height) // 2
+	if LowestOffs is None or Offset < LowestOffs:
+		LowestOffs = Offset
 
-# Close the font
+	NewImg = Image.new("L", (Img.width - WidthAdjust, ImgHeight), Color)
+	NewImg.paste(Img, (0, Offset - abs(LowestOffs)))
+	NewImg.save(OutputFile)
+
 Font.close()
