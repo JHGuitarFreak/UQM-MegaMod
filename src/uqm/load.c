085303d3e86af79affd61600097863c8d629cd38
JHGuitarFreak <jhguitarfreak@gmail.com>
2025-11-29 14:13:52 -0600

    Remove debugging printf from load.c

--- src/uqm/load.c
+++ src/uqm/load.c
@@ -472,8 +472,6 @@ LoadSisState (SIS_STATE *SSPtr, void *fp, BOOLEAN try_core,
  	{
 		if (try_core)
 		{	// Use old Log X to Universe code to get proper coordinates from core saves
-			printf ("SSPtr->log_x %d, oldLogxToUniverse () %d, UNIVERSE_TO_LOGX () %d\n", SSPtr->log_x, oldLogxToUniverse (SSPtr->log_x), UNIVERSE_TO_LOGX (oldLogxToUniverse (SSPtr->log_x)));
-
 			SSPtr->log_x = UNIVERSE_TO_LOGX (oldLogxToUniverse (SSPtr->log_x));
 			SSPtr->log_y = UNIVERSE_TO_LOGY (oldLogyToUniverse (SSPtr->log_y));
 		}
