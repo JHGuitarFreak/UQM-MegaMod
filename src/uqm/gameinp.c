c0e50ac43a69293cbe94390b76975fc825ab5a68
JHGuitarFreak <jhguitarfreak@gmail.com>
2026-08-08 13:59:57 -0500

    Fix merge resolve errors

--- src/uqm/gameinp.c
+++ src/uqm/gameinp.c
@@ -38,11 +38,8 @@
 #include "setupmenu.h"
 #include "libs/graphics/gfx_common.h"
 #include "gameopt.h"
-<<<<<<< HEAD
 #include "libs/log/uqmlog.h"
-=======
 #include "journal.h"
->>>>>>> ReJournal
 
 #define ACCELERATION_INCREMENT (ONE_SECOND / 12)
 #define MENU_REPEAT_DELAY (ONE_SECOND >> 1)
@@ -311,13 +308,11 @@ UpdateInputState (void)
 			QuickSave ();
 	}
 
-<<<<<<< HEAD
 	if (optAutoButtons && !InSetupMenu)
 		ControllerTypeSwitcher ();
-=======
+
 	if (PulsedInputState.menu[KEY_JOURNAL])
 		JournalRequested = TRUE;
->>>>>>> ReJournal
 
 #if defined(DEBUG) || defined(USE_DEBUG_KEY)
 	if (PulsedInputState.menu[KEY_DEBUG])
